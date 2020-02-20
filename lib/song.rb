514424a75a3ae561e04f6ee3d9355b485bfed147
Vincent Servio <vincentservio@gmail.com>
2020-02-20 20:48:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,4 +26,16 @@ class Song
   def self.genres 
     @@genres.uniq
   end
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre| 
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else 
+        genre_count[genre] = 1 
+      end 
+      
+  end 
+  genre_count
+end 
 end 
