659487ed2477071914693cb897c4bffef39b17da
Vincent Servio <vincentservio@gmail.com>
2020-02-20 20:37:06 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+require 'pry'
+
+class Song 
+  
+  attr_accessor :name, :artist, :genre
+  @@count = 0 
+  @@artists = []
+   @@genres =[]
+  
+  def initialize(name,artist,genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+  @@genres << genre
+  end
+  
+  def self.count 
+    @@count
+
+  end 
+  def self.artists
+    @@artists
+  end
+end 
